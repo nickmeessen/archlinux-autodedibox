c3545ee885f792d87adc39973476f750f1e68168
Nick Meessen <nickmeessen@users.noreply.github.com>
2015-04-15 13:07:48 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -20,8 +20,17 @@ pacstrap /mnt base base-devel syslinux zsh openssh
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
 # we get the script that will be used next
-wget http://dl.leneveu.fr/public/autoconfigure.sh
-mv autoconfigure.sh /mnt
+wget https://raw.githubusercontent.com/nickmeessen/archlinux-autodedibox/master/autoconfigure.sh
+mv autoconfigure.sh /mnt/tmp/
 
-# we chroot to real system
-arch-chroot /mnt
+# we chroot into temp arch environment
+cd /mnt
+cp /etc/resolv.conf etc
+mount -t proc /proc proc
+mount --rbind /sys sys
+mount --rbind /dev dev
+mount --rbind /run run
+# (assuming /run exists on the system)
+chroot /mnt /bin/bash
+
+sh /mnt/tmp/autoconfigure.sh
